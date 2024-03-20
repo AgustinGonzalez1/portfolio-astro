35828250e19dbe27b54f5dc020cb51c4a6dc57f1
Agustin Gonzalez <agustin_.gonzalez_@hotmail.com>
2024-03-20 14:15:33 -0300

    created types, projects, cardprojects

---
+++ src/types/projects.ts
@@ -0,0 +1,8 @@
+export type Project = {
+  title: string;
+  description: string;
+  link: string;
+  img: string;
+  year: number;
+  role: string;
+};
---
+++ src/utils/arrProjects.ts
@@ -0,0 +1,76 @@
+import { type Project } from "../types/projects";
+
+export const arrProjects: Project[] = [
+  {
+    title: "Proyecto Final de Desarrollo Web",
+    description:
+      "Mi introducción al desarrollo web comenzó con éxito al crear una página web destacada en mi curso, lo que me llevó al top 10 de mi clase y me inspiró a seguir explorando este campo.",
+    link: "https://guarida-gamer.netlify.app/",
+    img: "/imgProjects/guaridaGamer.jpg",
+    year: 2023,
+    role: "Frontend",
+  },
+  {
+    title: "Proyecto Final de JavaScript 1/2",
+    description:
+      "Mi proyecto final se centró en JavaScript, donde logré estar entre los diez mejores y consolidé mi pasión por este lenguaje. Creé un sitio eCommerce interactivo que permitía a los usuarios explorar y gestionar su carrito de compras. Mi amor por JavaScript se reflejó en cada línea de código, marcando el inicio de mi emocionante viaje en el desarrollo web.",
+    link: "https://tienda-el-clima.netlify.app/",
+    img: "/imgProjects/JavaScriptn1.jpg",
+    year: 2023,
+    role: "Frontend",
+  },
+  {
+    title: "Proyecto Final de JavaScript 2/2",
+    description:
+      "En mi proyecto siguiente, creé una Aplicación Meteorológica con JavaScript, aprovechando la API de AccuWeather. Esta app recopila datos climáticos basados en la ubicación del usuario y los presenta de forma visualmente atractiva, utilizando colores y fotos descriptivas para representar intuitivamente las temperaturas y las condiciones climáticas. Esto resaltó mi habilidad para convertir datos en una experiencia agradable y funcional.",
+    link: "https://el-clima-app.netlify.app/",
+    img: "/imgProjects/JavaScriptn2.jpg",
+    year: 2023,
+    role: "Frontend",
+  },
+  {
+    title: "Proyecto Final de React",
+    description:
+      "Mi proyecto final en React demuestra mi pasión por este framework y mi capacidad para crear soluciones dinámicas. Creé un eCommerce completo que permite a los usuarios agregar, eliminar y comprar productos, integrando Firebase para un proceso de compra fluido con actualización en tiempo real del stock. Este proyecto me ubicó en el top 10 de mi curso y refleja mi habilidad para convertir ideas en experiencias interactivas y funcionales.",
+    link: "https://sportify-tienda.netlify.app/",
+    img: "/imgProjects/react.jpg",
+    year: 2023,
+    role: "Frontend - Backend(Firebase)",
+  },
+  {
+    title: "Proyecto top 10 React",
+    description:
+      "Este proyecto se me otorgo gracias al top 10 en React en el curso de CoderHouse, donde ejercí el rol de desarrollador FrontEnd junto con 3 diseñadoras y una product manager, adquirí mucha experiencia nueva y lo mas valioso de formar un grupo de trabajo, aprendiendo SCRUM",
+    link: "https://muebles-a-medida.netlify.app/",
+    img: "/imgProjects/muebles.jpg",
+    year: 2023,
+    role: "Frontend",
+  },
+  {
+    title: "TaskMaster",
+    description:
+      "En este proyecto de práctica, desarrollé tanto el FrontEnd como el BackEnd, implementando tokens de seguridad para proteger las rutas, manejando errores para garantizar un funcionamiento adecuado y mejorando mis habilidades en las tecnologías utilizadas para adquirir más experiencia.",
+    link: "https://taskmasterhub.vercel.app/",
+    img: "/imgProjects/taskMaster.jpg",
+    year: 2023,
+    role: "FullStack",
+  },
+  {
+    title: "Corona",
+    description:
+      "Gracias a este proyecto, logré entrar nuevamente al top 10 en CoderHouse. Fue una experiencia invaluable que me permitió profundizar aún más en Next.js y Firebase, además de aplicar buenas prácticas de desarrollo.",
+    link: "https://corona-ecommerce.vercel.app/",
+    img: "/imgProjects/corona.jpg",
+    year: 2024,
+    role: "Frontend",
+  },
+  {
+    title: "Rastrea Patitas",
+    description:
+      "Proyecto del Seleccionado 12 de NoCountry, donde fui parte de un grupo de 14 personas y mi rol era el de Front End. Fue una experiencia muy valiosa para seguir aprendiendo y mejorar mis habilidades en React.",
+    link: "https://rastreapatitas.vercel.app/",
+    img: "/imgProjects/rastreaPatitas.jpg",
+    year: 2024,
+    role: "Frontend",
+  },
+];
