98f5230a7a58324e4d40385e4c0fa2c36fe0ddd5
Agustin Gonzalez <agustin_.gonzalez_@hotmail.com>
2024-03-22 14:13:41 -0300

    finish about page

---
+++ src/utils/arrSkills.ts
@@ -0,0 +1,52 @@
+import { type Skills } from "../types/skills";
+
+export const arrSkills: Skills[] = [
+  {
+    name: "HTML",
+  },
+  {
+    name: "CSS",
+  },
+  {
+    name: "TailwindCSS",
+  },
+  {
+    name: "Bootstrap",
+  },
+  {
+    name: "SASS",
+  },
+  {
+    name: "Git",
+  },
+  {
+    name: "GitHub",
+  },
+  {
+    name: "JavaScript",
+  },
+  {
+    name: "TypeScript",
+  },
+  {
+    name: "React",
+  },
+  {
+    name: "NextJS",
+  },
+  {
+    name: "Astro",
+  },
+  {
+    name: "Firebase",
+  },
+  {
+    name: "Figma",
+  },
+  {
+    name: "NodeJS",
+  },
+  {
+    name: "ExpressJS",
+  },
+];
