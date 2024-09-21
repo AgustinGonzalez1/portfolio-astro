a665c842f7777729b93c3ce8b99f4bca038e64c1
Agustin Gonzalez <agustin_.gonzalez_@hotmail.com>
2024-09-20 23:30:47 -0300

    add: framer motion in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,45 @@
-
-import {motion, } from 'framer-motion'
+import { motion } from "framer-motion";
 
 export default function Header() {
-
-
-
-
   return (
     <header className="flex flex-col gap-16 md:flex-row md:gap-20 pt-10">
       <section className="flex flex-col gap-8 justify-center">
-        <motion.div initial={{ y: -100, opacity: 0 }} whileInView={{ y: 0, opacity: 1 }} transition={{ duration: 0.5, ease: 'easeInOut' }} viewport={{ once: true }} className="flex flex-col gap-3">
-          <h1 className="text-5xl font-bebas text-white xl:text-8xl">
+        <div className="flex flex-col gap-3">
+          <motion.h1
+            initial={{ x: -100, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.5, ease: "easeInOut" }}
+            viewport={{ once: true }}
+            className="text-5xl font-bebas text-white xl:text-8xl"
+          >
             Hola, soy <br /> Ricardo Gonzalez.
-          </h1>
-          <p className="text-subtext font-manrope lg:text-lg">
+          </motion.h1>
+          <motion.p
+            initial={{ x: -100, opacity: 0 }}
+            whileInView={{ x: 0, opacity: 1 }}
+            transition={{ duration: 0.5, ease: "easeInOut", delay: 0.1 }}
+            viewport={{ once: true }}
+            className="text-subtext font-manrope lg:text-lg"
+          >
             Docente Front-End en Play Code Latam. Desarrollador Full Stack PERN
             en ByDerm. Apasionado por crear sitios web accesibles y fáciles de
             usar.
-          </p>
-        </motion.div>
-        <div className="flex gap-4">
-            <a
-              href="#contact"
-              className="font-manrope flex items-center font-bold bg-primary border-2 border-primary hover:bg-transparent hover:text-white duration-200 rounded-full p-1 px-4 gap-3 text-sm"
-            >
-              <p>CONTACTAME</p>
-            </a>
-          
+          </motion.p>
+        </div>
+        <motion.div
+          initial={{ x: -100, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.5, ease: "easeInOut", delay: 0.2 }}
+          viewport={{ once: true }}
+          className="flex gap-4"
+        >
+          <a
+            href="#contact"
+            className="font-manrope flex items-center font-bold bg-primary border-2 border-primary hover:bg-transparent hover:text-white duration-200 rounded-full p-1 px-4 gap-3 text-sm"
+          >
+            <p>CONTACTAME</p>
+          </a>
+
           <a
             href="https://www.linkedin.com/in/ricardoagustingonzalez/"
             target="_blank"
@@ -43,10 +56,17 @@ export default function Header() {
           >
             <img src="/github.svg" alt="" />
           </a>
-        </div>
+        </motion.div>
       </section>
       <section className="mx-auto max-w-80 sm:max-w-[400px] md:max-w-[600px]">
-        <img src="/profile.png" alt="" />
+        <motion.img
+          initial={{ x: 100, opacity: 0 }}
+          whileInView={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.5, ease: "easeInOut", delay: 0.3 }}
+          viewport={{ once: true }}
+          src="/profile.png"
+          alt=""
+        />
       </section>
     </header>
   );
