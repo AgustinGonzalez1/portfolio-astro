af92d6089478ed86f2672e1b0b9c19dc7c3fbfed
Agustin <agustin_.gonzalez_@hotmail.com>
2025-10-06 09:06:53 -0300

    feat: add new contributions and images for Sanity integration and study reports

--- src/utils/arrContributions.ts
+++ src/utils/arrContributions.ts
@@ -10,23 +10,30 @@ export const arrContributions: Contribution[] = [
   },
   {
     title: "Formularios dinámicos - Frappe/Vue",
-    description: "Implementé en Python y JavaScript sobre el framework Frappe una función `createOrEdit` que, junto con `make_quick_entry`, genera automáticamente formularios dinámicos a partir de Doctypes predefinidos. Esto permite crear o editar entidades (pacientes, doctores) con la misma lógica reutilizable, optimizando tiempos y evitando duplicar código.",
+    description:
+      "Implementé en Python y JavaScript sobre el framework Frappe una función `createOrEdit` que, junto con `make_quick_entry`, genera automáticamente formularios dinámicos a partir de Doctypes predefinidos. Esto permite crear o editar entidades (pacientes, doctores) con la misma lógica reutilizable, optimizando tiempos y evitando duplicar código.",
     link: "https://www.linkedin.com/posts/ricardoagustingonzalez_frappe-vue-erpnext-activity-7286115491251638272-3H5r?utm_source=share&utm_medium=member_desktop&rcm=ACoAAD8QU50BytWNd4NLrZko8r75tzZatItiRF0",
-    img: "/imgContributions/quickNovigo.png"
+    img: "/imgContributions/quickNovigo.png",
   },
   {
     title: "Profesor en Play Code - HTML/CSS/JS",
-    description: "Renové mi rol como profesor en Play Code por otro año, impartiendo clases de programación y desarrollo de videojuegos, compartiendo mi experiencia con los alumnos y formando parte de un equipo de instructores de primer nivel.",
+    description:
+      "Renové mi rol como profesor en Play Code por otro año, impartiendo clases de programación y desarrollo de videojuegos, compartiendo mi experiencia con los alumnos y formando parte de un equipo de instructores de primer nivel.",
     link: "https://www.linkedin.com/posts/ricardoagustingonzalez_programaciaejn-desarrolloweb-desarrollodevideojuegos-activity-7316153534901633025-N9K_?utm_source=share&utm_medium=member_desktop&rcm=ACoAAD8QU50BytWNd4NLrZko8r75tzZatItiRF0",
-    img: "/imgContributions/profPlaycode.jpeg"
+    img: "/imgContributions/profPlaycode.jpeg",
   },
   {
     title: "Vista de reporte de estudios",
-    description: "Desarrollé una vista de reporte de estudios con tres paneles: selección de plantillas, editor Tiptap y campos dinámicos; separé la lógica en módulos independientes y sincronizo la plantilla activa mediante emits. Esto mejora el rendimiento, maximiza la reutilización de código y permite generar reportes más rápido con un componente genérico.",
-    link: "https://www.linkedin.com/posts/ricardoagustingonzalez_vuejs-vue3-frontenddeveloper-activity-7327100525466816512-Zd9i?utm_source=share&utm_medium=member_desktop&rcm=ACoAAD8QU50BytWNd4NLrZko8r75tzZatItiRF0",
-    img: "/imgContributions/studyNovigo.png"
-  }
-
-
-
+    description:
+      "Desarrollé una vista de reporte de estudios con tres paneles: selección de plantillas, editor Tiptap y campos dinámicos; separé la lógica en módulos independientes y sincronizo la plantilla activa mediante emits. Esto mejora el rendimiento, maximiza la reutilización de código y permite generar reportes más rápido con un componente genérico.",
+    link: "https://www.linkedin.com/posts/ricardoagustingonzalez_%C3%BAltimo-vistazo-antes-de-la-nueva-interfaz-activity-7378858507015639040-OyzA?utm_source=share&utm_medium=member_desktop&rcm=ACoAAD8QU50BytWNd4NLrZko8r75tzZatItiRF0",
+    img: "/imgContributions/studyNovigo.png",
+  },
+  {
+    title: "Integracion de Sanity a web de noticias - Astro",
+    description:
+      "Integré Sanity CMS en Grupo Gampi para gestionar contenidos dinámicos, creando schemas de autores y noticias vinculados. Implementé un sistema que permite crear noticias desde el CMS y visualizarlas automáticamente en el frontend con Astro, aprovechando las actualizaciones en tiempo real y la flexibilidad de Sanity para desarrollar portales de contenido escalables.",
+    link: "https://www.linkedin.com/posts/ricardoagustingonzalez_este-mes-en-grupo-gampi-aprend%C3%AD-a-usar-activity-7366560279058407424-CzxX?utm_source=share&utm_medium=member_desktop&rcm=ACoAAD8QU50BytWNd4NLrZko8r75tzZatItiRF0",
+    img: "/imgContributions/sanityAstro.png",
+  },
 ];
